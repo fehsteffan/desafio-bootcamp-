fdc2333906abf1a86884c9713e90d48562986a73
Felipe Steffan <felip.developer@hotmail.com>
2022-11-30 21:25:53 -0300

    backend project created

---
+++ backend/src/main/java/com/example/desafiobc/desafiobc/DesafiobcApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafiobc.desafiobc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiobcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiobcApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/desafiobc/desafiobc/DesafiobcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafiobc.desafiobc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiobcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
